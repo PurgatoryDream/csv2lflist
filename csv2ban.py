bb53a6f48dfd29e52d757591606f57eb6910c66e
PurgatoryDream <93832054+PurgatoryDream@users.noreply.github.com>
2021-11-06 18:14:21 +0100

    Updated csv2ban.py; whitelist + quantity fixes!

--- csv2ban.py
+++ csv2ban.py
@@ -4,21 +4,42 @@ from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
 
+# Loads the dataset and reads through it to create the file:
 def load_dataset(path):
-    f = open("collection.lflist.conf", "w")
+    f = open(basename(path).split('.')[0] + ".lflist.conf", "w")
     f.write("#[" + basename(path).split('.')[0] + " Banlist]" + "\n")
     f.write("!" + basename(path).split('.')[0] + " Banlist" + "\n")
+    f.write("$whitelist\n")
     f.write("#" + basename(path).split('.')[0] + " Banlist" + "\n")
 
     with open(path, 'r') as file:
         reader = csv.reader(file, delimiter=',')
+
+        # Set variables in case of multiple versions of the same card:
+        currentId = -1
+        quantity = 0
+        line = ""
         for row in reader:
-            line = ""
             if row[0] == "cardname":
                 continue
-            if int(row[1]) < 3:
-                line = str(row[2]) + " " + str(row[1]) + " --" + str(row[0]) + "\n"
-            f.write(line)
+
+            # If the last card was the same one (diff. version), accumulate quantity.
+            # Only write the line if the last one read was different.
+            if int(row[2]) == currentId:
+                quantity = quantity + int(row[1])
+            else:
+                # Save current Id and Quantity to compare with previous scenarios:
+                currentId = int(row[2])
+                quantity = int(row[1])
+                f.write(line)
+
+            if quantity > 3:
+                quantity = 3
+
+            line = str(currentId) + " " + str(quantity) + " --" + str(row[0]) + "\n"
+
+        # Write the last line in the end, or else the last one won't get printed.
+        f.write(line)
 
     f.close()
 
